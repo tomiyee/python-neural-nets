64fd775f79884dc5a5cd07d32ebf3b498a7c07f2
tomiyee <tommy.and.heng@gmail.com>
2017-08-10 21:40:49 -0700

    before starting

---
+++ Chatbot+Alpha.py
@@ -0,0 +1,83 @@
+
+# coding: utf-8
+
+# # Tommy's Auto Responder v0.1
+
+# In[2]:
+
+
+import json
+import requests
+import time
+import random
+
+TOKEN = "413228111:AAHdzFBSgxEVmsULUF302VUn0pRVw2ublao"
+URL = "https://api.telegram.org/bot{}/".format(TOKEN)
+
+options = ['Mhmm', 'yes', 'interesting.']
+
+def get_url(url):
+    response = requests.get(url)
+    content = response.content.decode("utf8")
+    return content
+
+
+def get_json_from_url(url):
+    content = get_url(url)
+    js = json.loads(content)
+    return js
+
+
+def get_last_update_id(updates):
+    update_ids = []
+    for update in updates["result"]:
+        update_ids.append(int(update["update_id"]))
+    return max(update_ids)
+
+
+def get_updates(offset=None):
+    url = URL + "getUpdates"
+    if offset:
+        url += "?offset={}".format(offset)
+    js = get_json_from_url(url)
+    return js
+
+
+def get_last_chat_id_and_text(updates):
+    num_updates = len(updates["result"])
+    last_update = num_updates - 1
+    text = updates["result"][last_update]["message"]["text"]
+    chat_id = updates["result"][last_update]["message"]["chat"]["id"]
+    return (text, chat_id)
+
+
+def send_message(text, chat_id):
+    url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
+    get_url(url)
+    
+    
+def main():
+    last_textchat = (None, None)
+    while True:
+        text, chat = get_last_chat_id_and_text(get_updates())
+        if (text, chat) != last_textchat:
+            send_message(options[random.randint(0, len(options)-1)], chat)
+            last_textchat = (text, chat)
+        time.sleep(0.5)
+
+
+if __name__ == '__main__':
+    main()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
